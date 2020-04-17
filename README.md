eab46b96e820e23f31bd5c560fa259161d0d8fe1
zguang <zguang.loh@gmail.com>
2020-04-17 13:54:26 +0800

    Customizing HTTP Requests

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Weather-App
+
+1. Asynchronous Basics
+2. Call Stack, Callback Queue, and Event Loop
+3. Making HTTP Requests
+4. Customizing HTTP Requests
--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-console.log("Starting");
-setTimeout(() => {
-  console.log("2 Second Timer");
-}, 2000);
+const request = require("request");
 
-setTimeout(() => {
-  console.log("0 Second Timer");
-}, 0);
-console.log("Ending");
+// const url =
+//   "https://api.data.gov.sg/v1/environment/air-temperature?date=2020-04-17";
+
+const url =
+  "http://api.weatherstack.com/current?access_key=ad85093133574d10ebff376dc6e2e87f&query=New York";
+
+request({ url: url, json: true }, (error, response) => {
+  console.log(
+    response.body.current.weather_descriptions[0] +
+      ". It is currently " +
+      response.body.current.temperature +
+      " degree at " +
+      response.body.location.name +
+      "."
+  );
+});

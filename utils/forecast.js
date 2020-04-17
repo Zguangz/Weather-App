346de6d421ba8b590f54b7e81e9a5dbcc698af1b
zguang <zguang.loh@gmail.com>
2020-04-17 19:30:31 +0800

    DestructuringDestructuring and Property

--- utils/forecast.js
+++ utils/forecast.js
@@ -10,21 +10,21 @@ const forecast = (latitude, longitude, callback) => {
     "," +
     longitude;
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to weather service!", undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback("Unable to find location.", undefined);
     } else {
       callback(
         undefined,
-        response.body.current.weather_descriptions[0] +
+        body.current.weather_descriptions[0] +
           ". It is currently " +
-          response.body.current.temperature +
+          body.current.temperature +
           " degree at " +
-          response.body.location.name +
+          body.location.name +
           " but feels like " +
-          response.body.current.feelslike +
+          body.current.feelslike +
           " degree."
       );
     }

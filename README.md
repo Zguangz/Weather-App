6c4dbb478d2e2c7d3e1b5d84efef4933c418f297
zguang <zguang.loh@gmail.com>
2020-04-17 14:48:29 +0800

    Handling Errors

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 2. Call Stack, Callback Queue, and Event Loop
 3. Making HTTP Requests
 4. Customizing HTTP Requests
+5. An HTTP Request Challenge
+6. Handling Errors
+7. The Callback Function
+8. Callback Abstraction
+9. Callback Abstraction Challenge
+10. Callback Chaining
+11. ES6 Aside: Object Property Shorthand and Destructuring
+12. Destructuring and Property Shorthand Challenge
+13. HTTP Requests without a Library
--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 const request = require("request");
 
 // const url =
-//   "https://api.data.gov.sg/v1/environment/air-temperature?date=2020-04-17";
-
-// const url =
-//   "http://api.weatherstack.com/current?access_key=ad85093133574d10ebff376dc6e2e87f&query=New York";
+//   "http://api.weatherstack.com/current?access_key=ad85093133574d10ebff376dc6e2e87f&query=33.652275,-84.121438";
 
 // request({ url: url, json: true }, (error, response) => {
-//   console.log(
-//     response.body.current.weather_descriptions[0] +
-//       ". It is currently " +
-//       response.body.current.temperature +
-//       " degree at " +
-//       response.body.location.name +
-//       "."
-//   );
+//   if (error) {
+//     console.log("Unable to connect to weather service!");
+//   } else if (response.body.error) {
+//     console.log("Unable to find location");
+//   } else {
+//     console.log(
+//       response.body.current.weather_descriptions[0] +
+//         ". It is currently " +
+//         response.body.current.temperature +
+//         " degree at " +
+//         response.body.location.name +
+//         "."
+//     );
+//   }
 // });
 
 // Geocoding
 const geoCodeURL =
-  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibHpndWFuZ3psIiwiYSI6ImNrOTNzOXpvbTA1bTMzZG9scGRrdDR4bGUifQ.Ehg8tUQVwOuAgrNnwmb-9g&limit=1";
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/philadelpah.json?access_token=pk.eyJ1IjoibHpndWFuZ3psIiwiYSI6ImNrOTNzOXpvbTA1bTMzZG9scGRrdDR4bGUifQ.Ehg8tUQVwOuAgrNnwmb-9g&limit=1";
 request({ url: geoCodeURL, json: true }, (error, response) => {
-  const latitude = response.body.features[0].center[1];
-  const longitude = response.body.features[0].center[0];
-  console.log(latitude, longitude);
+  if (error) {
+    console.log("Unable to connect to location service!");
+  } else if (response.body.features.length === 0) {
+    console.log("Unable to find location. Try another search.");
+  } else {
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    console.log(latitude, longitude);
+  }
 });
